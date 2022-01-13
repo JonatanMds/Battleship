a6f7fcb6a6113bcbd8295983120c3ed3a569459b
Jonatan Marques <marques.jonatands@gmail.com>
2022-01-13 12:21:48 -0300

    Adicionado Regras de jogos, mais atualização da maneira de jogar do machine

---
+++ src/GameRules.java
@@ -0,0 +1,18 @@
+public class GameRules {
+    public void Regras(){
+        System.out.println(
+                        " _           _   _   _           _     _       \n" +
+                                "| |         | | | | | |         | |   (_)      \n" +
+                                "| |__   __ _| |_| |_| | ___  ___| |__  _ _ __  \n" +
+                                "| '_ \\ / _` | __| __| |/ _ \\/ __| '_ \\| | '_ \\ \n" +
+                                "| |_) | (_| | |_| |_| |  __/\\__ \\ | | | | |_) |\n" +
+                                "|_.__/ \\__,_|\\__|\\__|_|\\___||___/_| |_|_| .__/ \n" +
+                                "                                        | |    \n" +
+                                "                                        |_|    \n" +
+                        "Nessa versão o jogador terá a apenas 10 submarino para posicionar pelo seu mata \n"+
+                        "Navio posicionado N (Maiúsculo)\n" +
+                                ". Tiro certeiro * (Asterisco)\n" +
+                                ". Tiro na água – (Traço)\n" +
+                                ". Tiro certeiro com navio posicionado X (Maiúsculo)\n");
+    }
+}
